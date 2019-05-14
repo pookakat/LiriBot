8d1b9fb2c8c31d5550cacfd29aac5168788f2e1e
Katie Humburg <pookakat@gmail.com>
2019-05-13 20:33:49 -0400

    finish basic setup with Git Lab

---
+++ liri.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+commands = [concert-this, spotify-this-sessionStorage, movie-this, do-what-it-says];
