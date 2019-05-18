823ee750d1b66a1f33f5f669c9e444b6b6855e1c
Katie Humburg <pookakat@gmail.com>
2019-05-17 22:03:44 -0400

    make the thing take the whole title or band name and put it in the switch stuff

--- liri.js
+++ liri.js
@@ -2,25 +2,42 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-commands = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'];
+var nextThing = "";
+if (process.argv.length>3){
+    for (i=3; i<process.argv.length; i++){
+      nextThing = nextThing + " " + process.argv[i];
+    }
+  }
+  else{
+    nextThing = process.argv[3];
+  }
+commands = ['concert-this', 'spotify-this-song', 'nextThing-this', 'do-what-it-says', 'moo'];
 
-var doThis = process.argv[2];
+function commandPrompt(title){
+    var doThis = process.argv[2];
 
-switch (doThis) {
-    case commands[0]:{
-        console.log(commands[0]);
-        break;
-    }
-    case commands[1]:{
-        console.log(commands[1]);
-        break;
-    }
-    case commands[2]:{
-        console.log(commands[2]);
-        break;
-    }
-    case commands[3]:{
-        console.log(commands[3]);
-        break;
+    switch (doThis) {
+        case commands[0]:{
+            console.log(commands[0]);
+            break;
+        }
+        case commands[1]:{
+            console.log(commands[1]);
+            break;
+        }
+        case commands[2]:{
+            console.log(commands[2]);
+            break;
+        }
+        case commands[3]:{
+            console.log(commands[3]);
+            break;
+        }
+        case commands[4]:{
+            console.log('You have no cows!');
+            console.log(title);
+            break;
+        }
     }
 }
+commandPrompt(nextThing);
