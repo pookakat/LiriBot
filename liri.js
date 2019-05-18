51749360b4ec53f1b77539fd486f676c1262763d
Katie Humburg <pookakat@gmail.com>
2019-05-17 22:34:28 -0400

    make omdb do stuff

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
+var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
 var nextThing = "";
 if (process.argv.length>3){
@@ -11,7 +12,7 @@ if (process.argv.length>3){
   else{
     nextThing = process.argv[3];
   }
-commands = ['concert-this', 'spotify-this-song', 'nextThing-this', 'do-what-it-says', 'moo'];
+commands = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says', 'moo'];
 
 function commandPrompt(title){
     var doThis = process.argv[2];
@@ -26,7 +27,17 @@ function commandPrompt(title){
             break;
         }
         case commands[2]:{
+            if (process.argv.length=3){
+                title = "Mr. Nobody";
+            }
+            console.log(nextThing);
+            //movie stuff
             console.log(commands[2]);
+            axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(
+            function(response) {
+            console.log("Title of the movie: " + response.data.Title + "\nYear the movie came out: " + response.data.Year + "\nIMDB Rating of the movie: " + response.data.imdbRating + "\nRotten Tomatoes Rating of the movie: " + response.data.ratings + "\nCountry where the movie was Produced: " + response.data.Country + "\nLanguage of the movie: " + response.data.Language + "\nPlot of the movie: " + response.data.Plot + "\nActors in the movie: " + response.data.Actors);
+  }
+);
             break;
         }
         case commands[3]:{
