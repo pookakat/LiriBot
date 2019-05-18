def0c957b1557660b611694c070c2140a98629a7
Katie Humburg <pookakat@gmail.com>
2019-05-17 23:41:16 -0400

    get the concert thing to work

--- liri.js
+++ liri.js
@@ -3,23 +3,31 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
 var nextThing = "";
 if (process.argv.length>3){
     for (i=3; i<process.argv.length; i++){
-      nextThing = nextThing + " " + process.argv[i];
+      nextThing = nextThing + process.argv[i] + " ";
     }
   }
   else{
     nextThing = process.argv[3];
+    nextThing = nextThing;
   }
 commands = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says', 'moo'];
 
 function commandPrompt(title){
     var doThis = process.argv[2];
-
+    title = title.trimEnd();
     switch (doThis) {
         case commands[0]:{
             console.log(commands[0]);
+            console.log(title);
+            axios.get("https://rest.bandsintown.com/artists/" + title + "/events?app_id=18982016-4489-49ce-9678-32dce1606300&date=upcoming").then(function(response) {
+                for (var i = 0; i<response.data.length; i++){
+                    console.log("Name of the Venue: " + response.data[i].venue.name + "\nVenue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + "\nDate of Event: " + moment(response.data[i].datetime).format("MM-DD-YYYY"));
+                }
+            });
             break;
         }
         case commands[1]:{
@@ -27,12 +35,9 @@ function commandPrompt(title){
             break;
         }
         case commands[2]:{
-            if (process.argv.length=3){
+            if (process.argv.length<=3){
                 title = "Mr. Nobody";
             }
-            console.log(nextThing);
-            //movie stuff
-            console.log(commands[2]);
             axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(
             function(response) {
             console.log("Title of the movie: " + response.data.Title + "\nYear the movie came out: " + response.data.Year + "\nIMDB Rating of the movie: " + response.data.imdbRating + "\nRotten Tomatoes Rating of the movie: " + response.data.ratings + "\nCountry where the movie was Produced: " + response.data.Country + "\nLanguage of the movie: " + response.data.Language + "\nPlot of the movie: " + response.data.Plot + "\nActors in the movie: " + response.data.Actors);
