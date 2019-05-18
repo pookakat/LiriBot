03c644b905175315e74471fda369d3fcc4b42d94
Katie Humburg <pookakat@gmail.com>
2019-05-17 21:46:53 -0400

    add stuff that I'm not sure what it does anymore

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-commands = [concert-this, spotify-this-sessionStorage, movie-this, do-what-it-says];
+var commands = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
